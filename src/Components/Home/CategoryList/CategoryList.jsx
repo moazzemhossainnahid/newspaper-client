4535827c4f8514119fd1c2f9ed2a2602354b6c44
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-05-13 15:00:00 +0600

    blog category add

--- src/Components/Home/CategoryList/CategoryList.jsx
+++ src/Components/Home/CategoryList/CategoryList.jsx
@@ -4,8 +4,61 @@ import { GrGallery } from 'react-icons/gr';
 import { SlCalender } from 'react-icons/sl';
 
 const CategoryList = () => {
+
+    const dataItems = [
+        {
+            id: 1,
+            label: 'Recent',
+            image: 'https://themes.zozothemes.com/newser/wp-content/uploads/2016/10/post-28-130x80.jpg',
+            title: 'Chinese food ingredients – What Chinese eat',
+            date: 'Oct 20, 2016',
+            comment: '120'
+        },
+        {
+            id: 2,
+            label: 'Trending',
+            image: 'https://themes.zozothemes.com/newser/wp-content/uploads/2016/10/post-23-130x80.jpg',
+            title: '8 Fantastic ways to improve your daily fitness',
+            date: 'Nov 12, 2028',
+            comment: '34'
+    
+        },
+        {
+            id: 3,
+            label: ' Most Liked',
+            image: 'https://themes.zozothemes.com/newser/wp-content/uploads/2016/10/post-21-130x80.jpg',
+            title: 'Effective ways to get more out of basketball',
+            date: 'Dec 23, 2015',
+            comment: '321'
+        },
+        {
+            id: 3,
+            label: ' Most Liked',
+            image: 'https://themes.zozothemes.com/newser/wp-content/uploads/2016/10/post-29-130x80.jpg',
+            title: 'Healthy fish recipes for crazy-busy week nights',
+            date: 'Oct 17, 2016',
+            comment: '321'
+        },
+        {
+            id: 3,
+            label: ' Most Liked',
+            image: 'https://themes.zozothemes.com/newser/wp-content/uploads/2016/10/post-20-130x80.jpg',
+            title: 'NBA Schedule: Times, TV Listings for Oct',
+            date: 'Dec 17, 2019',
+            comment: '321'
+        },
+        {
+            id: 3,
+            label: ' Most Liked',
+            image: 'https://themes.zozothemes.com/newser/wp-content/uploads/2016/11/post-276-130x80.jpg',
+            title: 'Top ten and Best Beauty Looks for this year',
+            date: 'Oct 19, 2016',
+            comment: '321'
+        },
+    ]
+
     return (
-        <div className='w-full h-full'>
+        <div className='w-full h-full p-5'>
             <div className='flex flex-col lg:flex-row justify-around gap-10 items-center'>
                 <div>
                     <div>
@@ -71,6 +124,28 @@ const CategoryList = () => {
                             </ul>
                         </div>
                     </div>
+                </div>
+            </div>
+
+            <div className='my-10'>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 '>
+                    {dataItems.map((item) => (
+                        <div key={item.id} >
+                            <div className='flex justify-around gap-5 items-center  mb-3'>
+                                <div>
+                                    <img className='w-full' src={item.image} alt="" />
+                                </div>
+                                <div>
+                                    <h2 className='text-black text-sm font-serif hover:text-red-400   text-left'>{item.title}</h2>
+                                    {/* <p>{item.date}</p> */}
+                                    <span className='flex justify-between items-center'>
+                                        <li className='flex justify-start items-center text-xl mt-2 text-black'> <SlCalender className='mr-3' /> <a className='hover:text-red-900 text-sm text-black' href="">{item.date}</a></li>
+                                        {/* <li className='list-none mt-2 text-xl text-white -ml-10 hover:text-red-900'> <a href=""> <AiOutlineHeart /> </a></li> */}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
